9f3fcb30d6f80dc5c7d44d6b3b0cff8aa6ed04d0
David Collier <david_a_collier@hotmail.com>
2025-11-19 21:56:56 +1100

    Changed vite.config.js

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/electronic-trading-app/',
   plugins: [react(), tailwindcss()],
 })
